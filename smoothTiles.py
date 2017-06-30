b62b5a34d0fc2850f2003b95b5dfc0bcfe95c482
Peter Pfoertsch <ppfoerts@uwo.ca>
2017-06-30 12:29:35 -0400

    Finished Smoothing
    The smoothing is working well.

--- smoothTiles.py
+++ smoothTiles.py
@@ -4,22 +4,25 @@ import tkinter
 import random
 
 #list for the map
-virtualMap = [['blue' for x in range(500)] for y in range (500)]
+height = 500
+width = 500
+virtualMap = [['blue' for x in range(width)] for y in range (height)]
+
 
 def surroundingTiles(x,y):
     count = 0
     #get surrounding x and y tiles
     for adjx in range(x-1,x+2):
         for adjy in range(y-1,y+2):
-            if(adjx >= 0 and adjx < 500 and adjy >= 0 and adjy < 500):
+            if(adjx >= 0 and adjx < height and adjy >= 0 and adjy < width):
                 if(adjx != x or adjy != y):
                     if(virtualMap[adjx][adjy] == 'green'):
                         count += 1
     return count
     
 def smoothMap():
-    for x in range(0,500):
-        for y in range(0,500):
+    for x in range(0,height):
+        for y in range(0,width):
             neighbors = surroundingTiles(x,y)
             
             if(neighbors > 4):
@@ -28,11 +31,11 @@ def smoothMap():
                 virtualMap[x][y] = 'blue'
 
 if __name__ == "__main__":
-    for x in range(0,500):
-        for y in range(0,500):
-            r = random.randint(99,100)
+    for x in range(0,height):
+        for y in range(0,width):
+            r = random.randint(0,100)
             color = 'red'
-            if (r >= 100):
+            if (r >= 50):
                 color = 'green'
             else:
                 color = 'blue'
@@ -42,11 +45,12 @@ if __name__ == "__main__":
     #create canvas using tkinter
     haupt = tkinter.Tk()
     c = tkinter.Canvas(haupt, bg="white", height=500, width=500)
-    smoothMap()
-    smoothMap()
-            
-    for num in range(0,500):
-        for num2 in range(0,500):
+    
+    for i in range(0,5):
+        smoothMap()
+    
+    for num in range(0,height):
+        for num2 in range(0,width):
             shiftx = num * 5
             shifty = num2 * 5
             square = c.create_polygon(0+shiftx,0+shifty,0+shiftx,5+shifty,5+shiftx,5+shifty,5+shiftx,0+shifty,fill=virtualMap[num][num2])
