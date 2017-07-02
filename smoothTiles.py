abca930b7706b6190912c518428992ddb1924607
Peter Pfoertsch <ppfoerts@uwo.ca>
2017-07-01 22:41:07 -0400

    Failed to screenshot
    Tried and failed to add screenshot function, there just does not seem to
    be a way to efficiently get the canvas of tkinter.

--- smoothTiles.py
+++ smoothTiles.py
@@ -2,11 +2,16 @@
 
 import tkinter
 import random
+from PIL import Image
+import pyscreenshot as ImageGrab
+import time
 
 #list for the map
 height = 500
 width = 500
 virtualMap = [['blue' for x in range(width)] for y in range (height)]
+smoothness = 4
+white = (255,255,255)
 
 
 def surroundingTiles(x,y):
@@ -42,20 +47,36 @@ if __name__ == "__main__":
                 
             virtualMap[x][y] = color
             
+    
+    
+    for i in range(0,smoothness):
+        smoothMap()
+        
     #create canvas using tkinter
     haupt = tkinter.Tk()
-    c = tkinter.Canvas(haupt, bg="white", height=500, width=500)
-    
-    for i in range(0,5):
-        smoothMap()
+    c = tkinter.Canvas(haupt, bg="white", height=height, width=width)
     
     for num in range(0,height):
         for num2 in range(0,width):
             shiftx = num * 5
             shifty = num2 * 5
             square = c.create_polygon(0+shiftx,0+shifty,0+shiftx,5+shifty,5+shiftx,5+shifty,5+shiftx,0+shifty,fill=virtualMap[num][num2])
-            
-
+      
+        
+    #c.update()
+   
     c.pack()
 
+    #create image
+    #image1 = Image.new("RGB",(width,height),white)
+    #draw = ImageDraw.Draw(image1)
+    #time.sleep(3)
+    #x = 100
+    #y = 200
+    #print(x,y)
+    #filename = time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime())
+    #ImageGrab.grab(bbox=(x,y,x+width,y+height)).save(filename + '.jpg')
+    
+    
     haupt.mainloop()
+    
